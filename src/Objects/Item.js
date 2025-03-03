0312c3c639da43a4432e267a1b8b764d9d4fb1bf
Chaoss66-dev <erik.voss@spaceforce.mil>
2025-03-03 11:43:23 -0800

    item class basic functionality

---
+++ src/Objects/Item.js
@@ -0,0 +1,12 @@
+class Item {
+    constructor(data) {
+        this.name = data.name
+        this.id = data.id
+        this.price = data.cost
+        this.flavor_text = data.flavor_text_entries
+    }
+
+
+}
+
+export default Item
