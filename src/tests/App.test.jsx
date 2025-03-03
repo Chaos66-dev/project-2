3ef4000aa752f9b25f1e6682d8e4242fba9818fc
Chaoss66-dev <erik.voss@spaceforce.mil>
2025-03-03 12:21:53 -0800

    installing required testing packages and configuring them

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,4 +1,30 @@
+/* eslint-disable no-undef */
 // src/App.test.jsx
-import { render, screen, fireEvent } from "@testing-library/react";
-import App from "./App";
+import { render, screen } from "@testing-library/react";
+import App from "../App/App.jsx";
 
+describe("App", () => {
+    beforeEach(() => {
+      // Arrange
+      render(<App />);
+    });
+
+    test("Renders the App", () => {
+      // Act
+      //   No Act steps needed
+      // Assert
+      expect(screen.getByText("PokeCommerce")).toBeInTheDocument();
+    });
+
+    test("Renders the search bar", () => {
+      // Act
+      //   No Act steps needed
+      // Assert
+      //   Check if the initial count is 0
+      expect(screen.getByText("Search...")).toBeInTheDocument();
+    });
+
+    test("Renders our shopping card button", () => {
+        expect(screen.getByText("Cart")).toBeInTheDocument();
+    })
+  });
