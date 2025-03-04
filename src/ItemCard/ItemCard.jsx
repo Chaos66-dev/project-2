f40fbb81df01c88e0bfdef8d88fc05e2701e29f1
Chaoss66-dev <erik.voss@spaceforce.mil>
2025-03-04 09:40:13 -0800

    pulled in main branch and made minor style changes

--- src/ItemCard/ItemCard.jsx
+++ src/ItemCard/ItemCard.jsx
@@ -7,7 +7,7 @@ function ItemCard ({prop}) {
             <Link to={`/details/${prop.id}`}>
                 <div className='card-container'>
                     <div>
-                    {separateHyphens(prop.name)}
+                        {separateHyphens(prop.name)}
                     </div>
                     <div>
                         <img src={prop.sprites.default} className='gallery-img'/>
